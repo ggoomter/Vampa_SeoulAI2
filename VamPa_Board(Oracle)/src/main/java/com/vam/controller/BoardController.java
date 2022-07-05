43e16cccf60d24c39742fa315d84be9d7d26cf22
ggoomter <ggoomter@gmail.com>
2022-07-05 10:28:27 +0900

    글쓴이후 다시 리스트로 가기 완료

--- VamPa_Board(Oracle)/src/main/java/com/vam/controller/BoardController.java
+++ VamPa_Board(Oracle)/src/main/java/com/vam/controller/BoardController.java
@@ -2,20 +2,28 @@ package com.vam.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.vam.model.BoardVO;
+import com.vam.service.BoardService;
 
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
 	private static final Logger log = LoggerFactory.getLogger(BoardController.class);
 	
+	@Autowired
+    private BoardService bservice;
+	
 	@GetMapping("/list")
 	public String boardListGET() {
 		log.info("게시판 목록 페이지 진입");
-		return "list";
-		/* /WEB-INF/views/list.jsp */
+		return "board/list";
+		/* /WEB-INF/views/board/list.jsp */
 	}
 	
 	@GetMapping("/enroll")
@@ -28,4 +36,14 @@ public class BoardController {
 		log.info("커스덤 페이지 진입");
 		return "board/custom";	//void
 	}
+	
+    /* 게시판 등록처리 */
+    @PostMapping("/enroll")
+    public String boardEnrollPOST(BoardVO board) {
+        
+        log.info("BoardVO : " + board);
+        bservice.enroll(board);
+        return "redirect:/board/list";
+        
+    }
 }
