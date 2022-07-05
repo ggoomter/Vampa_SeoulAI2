1cc00aaf573190959e4ab8d864781b594548d6f3
ggoomter <ggoomter@gmail.com>
2022-07-05 10:36:21 +0900

    글쓰고 결과 alert로 띄우기

--- VamPa_Board(Oracle)/src/main/java/com/vam/controller/BoardController.java
+++ VamPa_Board(Oracle)/src/main/java/com/vam/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.vam.model.BoardVO;
 import com.vam.service.BoardService;
@@ -39,10 +40,11 @@ public class BoardController {
 	
     /* 게시판 등록처리 */
     @PostMapping("/enroll")
-    public String boardEnrollPOST(BoardVO board) {
+    public String boardEnrollPOST(BoardVO board, RedirectAttributes rttr) {
         
         log.info("BoardVO : " + board);
         bservice.enroll(board);
+        rttr.addFlashAttribute("result", "enrol success");
         return "redirect:/board/list";
         
     }
